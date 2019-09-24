ebadd77ca778cd0217f0cc457cd62e6f319c2af3
Yichen Wang <yicwang@cisco.com>
2019-09-23 17:09:13 -0700

    Fix eventlet thread for bining tap into Linux Bridge
    Change-Id: I9d3293b194f8628e2ac1d416ead7cb1d563ac56e

--- networking_vpp/agent/server.py
+++ networking_vpp/agent/server.py
@@ -32,7 +32,6 @@ eventlet.monkey_patch(thread=False)
 
 import binascii
 from collections import defaultdict
-from collections import deque
 from collections import namedtuple
 import etcd
 import eventlet.semaphore
@@ -390,8 +389,12 @@ class VPPForwarder(object):
         self.vhost_ready_callback = None
         eventlet.spawn_n(self.vhost_notify_thread)
 
+        # Thread to drain the queues for binding tap interfaces into Linux
+        # bridges
+        eventlet.spawn_n(self.tap_notify_thread)
+
         # External devices detected by the device monitor
-        self.external_devices = deque()
+        self.external_devices = eventlet.queue.Queue()
         # Device monitor to ensure the tap interfaces are plugged into the
         # right Linux bridge
         self.async_devmon = device_monitor_async.AsyncDeviceMonitor()
@@ -822,24 +825,23 @@ class VPPForwarder(object):
 
         # TODO(ijw) will act upon other mechanism drivers' taps
         # Add the detected external device to be handled by the port-watcher
-        if dev_name not in self.external_devices:
-            self.external_devices.append(dev_name)
+        self.external_devices.put(dev_name)
 
-    def maybe_ensure_external_devices(self):
+    def tap_notify_thread(self):
         """Ensure detected external tap devices are added to the bridge.
 
         All detected external devices are queued in the external_devices
         data set. So handle it in this method to ensure that these are added
         to the bridge.
         """
-        try:
-            for dev_name in self.external_devices.popleft():
+        while True:
+            try:
+                dev_name = self.external_devices.get()
                 port_id = dev_name[3:]
                 bridge_name = "br-%s" % port_id
                 self.ensure_tap_in_bridge(dev_name, bridge_name)
-        except IndexError:
-            # The device queue may be empty
-            pass
+            except Exception:
+                LOG.error("Error while binding tap interface %s", dev_name)
 
     def ensure_tap_in_bridge(self, tap_name, bridge_name):
         """Add a TAP device to a Linux kernel bridge
@@ -3280,7 +3282,6 @@ class PortWatcher(etcdutils.EtcdChangeWatcher):
             data['segmentation_id'],
             data  # TODO(ijw) convert incoming to security fmt
             )
-        self.data.vppf.maybe_ensure_external_devices()
         # While the bind might fail for one reason or another,
         # we have nothing we can do at this point.  We simply
         # decline to notify Nova the port is ready.
